881de586daf37d0380cabd02939dc6f02faa1667
Charles Sampaio <charles.sampaio@inter.co>
2023-11-19 20:22:51 -0300

    refactore inital class

--- src/main/java/br/com/app/stockdata/Application.java
+++ src/main/java/br/com/app/stockdata/Application.java
@@ -4,9 +4,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -19,10 +16,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @OpenAPIDefinition(info = @Info(title = "Stock Data Market", version = "2.0", description = "Generation auth"))
 @EnableScheduling
 @EnableJpaRepositories
-public class SpaceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpaceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 	@Bean
